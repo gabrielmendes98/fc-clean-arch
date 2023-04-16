f4f9e823418c722e6b8af9bfc15fad8e61c3aba1
Gabriel Santiago <gabrielmssantiago@gmail.com>
2023-04-16 20:39:27 -0300

    feat: adiciona rotas e testes e2e de products

---
+++ src/infrastructure/api/routes/product.route.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from "express";
+import ListProductsUseCase from "../../../usecase/product/list/list-products.usecase";
+import ProductRepository from "../../product/repository/sequelize/product.repository";
+import CreateProductUseCase from "../../../usecase/product/create/create-product.usecase";
+import { InputCreateProductDto } from "../../../usecase/product/create/create-product.dto";
+
+export const productRoute = express.Router();
+
+productRoute.post("/", async (req: Request, res: Response) => {
+  const usecase = new CreateProductUseCase(new ProductRepository());
+  try {
+    const productDto: InputCreateProductDto = {
+      name: req.body.name,
+      price: req.body.price,
+    };
+    const output = await usecase.execute(productDto);
+    res.send(output);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+productRoute.get("/", async (req: Request, res: Response) => {
+  const usecase = new ListProductsUseCase(new ProductRepository());
+  const output = await usecase.execute({});
+
+  res.send(output);
+});
