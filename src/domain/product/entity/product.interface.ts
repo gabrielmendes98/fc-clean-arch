444ce0e11054714223a6c25cf75e7eb1ea095358
Gabriel Santiago <gabrielmssantiago@gmail.com>
2023-04-16 23:04:03 -0300

    feat: adiciona yup como validator respeitando o notification pattern

--- src/domain/product/entity/product.interface.ts
+++ src/domain/product/entity/product.interface.ts
@@ -1,8 +1,10 @@
-export default interface ProductInterface {
+import Entity from "../../@shared/entity/entity.abstract";
+
+export default interface ProductInterface extends Entity {
   get id(): string;
   get name(): string;
   get price(): number;
   changeName(name: string): void;
   changePrice(price: number): void;
-  validate(): boolean;
+  validate(): void;
 }
