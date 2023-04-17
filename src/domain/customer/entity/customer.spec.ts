05e8840d3e87d845280354e6121fec22f4303e56
Gabriel Santiago <gabrielmssantiago@gmail.com>
2023-04-16 22:24:29 -0300

    feat: adiciona notificações de erros para suportar multiplos erros e adiciona essas validações à entidade de customer

--- src/domain/customer/entity/customer.spec.ts
+++ src/domain/customer/entity/customer.spec.ts
@@ -4,14 +4,20 @@ import Customer from "./customer";
 describe("Customer unit tests", () => {
   it("should throw error when id is empty", () => {
     expect(() => {
-      const customer = new Customer("", "John");
-    }).toThrowError("Id is required");
+      let customer = new Customer("", "John");
+    }).toThrowError("customer: Id is required");
   });
 
   it("should throw error when name is empty", () => {
     expect(() => {
-      const customer = new Customer("123", "");
-    }).toThrowError("Name is required");
+      let customer = new Customer("123", "");
+    }).toThrowError("customer: Name is required");
+  });
+
+  it("should throw error when name is and id are empty", () => {
+    expect(() => {
+      let customer = new Customer("", "");
+    }).toThrowError("customer: Id is required,customer: Name is required");
   });
 
   it("should change name", () => {
@@ -48,17 +54,6 @@ describe("Customer unit tests", () => {
     customer.deactivate();
 
     expect(customer.isActive()).toBe(false);
-  });
-
-  it("should add reward points", () => {
-    const customer = new Customer("1", "Customer 1");
-    expect(customer.rewardPoints).toBe(0);
-
-    customer.addRewardPoints(10);
-    expect(customer.rewardPoints).toBe(10);
-
-    customer.addRewardPoints(10);
-    expect(customer.rewardPoints).toBe(20);
   });
 
   it("should add reward points", () => {
